7b9b51b1634f9ea059e0055e6dfe52b34b546f2b
unknown <santiago.gallegoh@upb.edu.co>
2023-01-25 16:36:53 -0600

    commit1

---
+++ main.py
@@ -0,0 +1,28 @@
+# CLASES Y OBJETOS
+
+class profesiones:  # clase
+    # Objetos que pertenecen a la clase persona
+    # Atributos de la clase
+    Bomberos = 30
+    policias = ""
+    profesores = 2
+
+
+trabajo = profesiones()
+print(profesiones.profesores)
+
+#CREAR OBJETOS FUERA DE LA CLASE
+class nombres:
+    pass #esto indica que es el final de la clase
+victor=nombres()
+maria=nombres()
+sara=nombres()
+#ESTRUCTURA-> Objeto.atributo=valor
+victor.edad=27
+victor.pais="Colombia"
+maria.edad=41
+maria.pais="Argentina"
+print(victor.edad)
+
+
+
