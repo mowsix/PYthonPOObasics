9bc7bd97612b0c8fc6e3279602ac7eb51f5578dd
unknown <santiago.gallegoh@upb.edu.co>
2023-01-31 06:25:32 -0600

    commit 2

--- main.py
+++ main.py
@@ -25,4 +25,14 @@ maria.pais="Argentina"
 print(victor.edad)
 
 
+#METODOS
+#La palabra reservada def nos indica que vamos a crear un metodo seguido del nombre
+#La palabra reservada self la utilizamos para referirnos al objeto con la estructura self.nombre=algoritmo
+#utilizamos __init__ para inicializar
+class matematicas:
+    def __init__(self):
+        self.numero1=45
+        self.numero2=45
+s=matematicas()
+print(s.numero1+s.numero2)
 
